a0e40f0ad3e78e2ed4d103e54f08596bc3756921
camila_fiaminghi <camila.fiaminghi@gmail.com>
2019-02-12 12:06:44 -0200

    Add api token

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const RESTAPI_URL = 'https://api.github.com'
+const RESTAPI_AUTH = 'token dd0f9fb05e1398f0784dd208d44d141b46134d5f'
 
 // https://api.github.com/search/repositories?q=org:facebook+sort:stars&per_page=3
 // https://api.github.com/repos/facebook/react/branches
@@ -8,7 +9,8 @@ const RESTAPI_URL = 'https://api.github.com'
 export const getReposByOrg = (org, sort='starts') => {
 	return axios.get(`${RESTAPI_URL}/search/repositories?q=org:${org}+sort:stars&per_page=4`, {
 		headers: {
-			'Accept': 'application/json'
+			'Accept': 'application/json',
+			'Authorization': RESTAPI_AUTH
 		}
 	})
 	.then(response => response.data)
@@ -18,7 +20,8 @@ export const getReposByOrg = (org, sort='starts') => {
 export const getBranches = (action) => {
 	return axios.get(`${RESTAPI_URL}/repos/${action.org}/${action.repo}/branches`, {
 		headers: {
-			'Accept': 'application/json'
+			'Accept': 'application/json',
+			'Authorization': RESTAPI_AUTH
 		}
 	})
 	.then(response => response.data)
