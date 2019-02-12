da011b238cc4627269e344a7c5d049d263524ddd
camilafiaminghi <camila.fiaminghi@gmail.com>
2019-02-11 23:54:27 -0200

    Move home view to stateless component

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import FormSearch from './../containers/FormSearch'
 import Results from './../containers/Results'
 
-class HomeView extends Component {
-	render() {
-		return (
-			<div>
-				<header>
-					<h2>Search the repositories of an organization</h2>
-				</header>
-				<FormSearch />
-				<Results />
-			</div>
-		)
-	}
+const HomeView = () => {
+	return (
+		<div>
+			<header>
+				<h2>Search the repositories of an organization</h2>
+			</header>
+			<FormSearch />
+			<Results />
+		</div>
+	)
 }
 
 export default HomeView
